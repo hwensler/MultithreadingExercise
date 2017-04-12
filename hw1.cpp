b376e81dfe8ccc4dcb403d749122f2ec48e210ff
hw <heatherwensler@gmail.com>
2017-04-12 10:33:16 -0700

    Added creation of 20 threads as well as output for encountered errors.

--- hw1.cpp
+++ hw1.cpp
@@ -9,5 +9,25 @@
 #include <cstdlib>
 #include <pthread.h>
 
+//tell how many threads there are
+#define THREAD_COUNT 20
 
+int main(){
+    //create thread ids
+    pthread_t threads[THREAD_COUNT];
+
+    //create int to determine if creation was successful
+    int errorCheck;
+
+    for(int i = 0; i < THREAD_COUNT; i++){
+        cout << "creating thread, " << i << endl;
+        rc = pthread_create(&threads[i], NULL, CountVowels, (void *)i);
+
+        if (rc){
+            cout << "Error: unable to create thread," << rc << endl;
+            exit(-1);
+        }
+    }
+    pthread_exit(NULL);
+}
 
