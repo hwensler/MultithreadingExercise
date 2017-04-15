e27a34739e1e6d453847fb11c1b42e56afab7dc6
hw <heatherwensler@gmail.com>
2017-04-15 12:47:05 -0700

    Runs, but nothing is joined or summed together

--- hw1.cpp
+++ hw1.cpp
@@ -15,9 +15,21 @@
 
 using namespace std;
 
-int main() {
-    //create thread ids
-    pthread_t threads[THREAD_COUNT];
+//initialize counts for all vowels
+struct vowelCount{
+    string fileLocation;
+    int aCount;
+    int eCount;
+    int iCount;
+    int oCount;
+    int uCount;
+};
+
+//this is the function that counts vowels
+void * countTheVowels(void*arg){
+
+    //*p is a pointer to this vowelCount struct
+    struct vowelCount *p = (struct vowelCount *) arg;
 
     //open filestream
     fstream file;
@@ -26,54 +38,86 @@ int main() {
     char character;
 
     //open file
-    file.open("C:\\Users\\heath\\Documents\\Repositories\\20ThreadedHomework\\txt examples\\file1.txt");
+    file.open(p->fileLocation);
 
-    //initialize counts for all vowels
-    int aCount = 0;
-    int eCount = 0;
-    int iCount = 0;
-    int oCount = 0;
-    int uCount = 0;
-
-    //this is the function that counts vowels
+    //while the file is open
     while(!file.eof())
     {
+        //read the next character
         file.get(character);
+
+        //now see if it's a vowel. if it is, increment.
         if(tolower(character) == 'a'){
-            aCount +=1;
+            p->aCount +=1;
         }
         else if(tolower(character) == 'e'){
-            eCount +=1;
+            p->eCount +=1;
         }
         else if(tolower(character) == 'i'){
-            iCount +=1;
+            p->iCount +=1;
         }
         else if(tolower(character) == 'o'){
-            oCount +=1;
+            p->oCount +=1;
         }
         else if(tolower(character) == 'u'){
-            uCount +=1;
+            p->uCount +=1;
         }
     }
 
     //close the file
     file.close();
 
+    //free your variables
+    delete p;
+
+    //return
+    return NULL;
+}
+
+int main() {
+    //create thread ids
+    pthread_t threads[THREAD_COUNT];
+
+
+    //create an array of these counts for all 20 file
+    vowelCount allCounts[THREAD_COUNT];
+
+    //create an array of all the file locations
+    string fileLocations[THREAD_COUNT];
+
+    //load in file locations & initialize counts to 0
+    for (int i = 0; i < THREAD_COUNT; i++){
+        //later
+        //allcounts[i].fileLocation = fileLocations[i];
+        allCounts[i].fileLocation = "C:\\Users\\heath\\Documents\\Repositories\\20ThreadedHomework\\txt examples\\file1.txt";
+        allCounts[i].aCount = 0;
+        allCounts[i].eCount = 0;
+        allCounts[i].iCount = 0;
+        allCounts[i].oCount = 0;
+        allCounts[i].uCount = 0;
+    }
+
     //create int to determine if creation was successful
     int errorCheck;
 
-    //create 20 threads
-    for (int i = 1; i <= THREAD_COUNT; i++)
+    //create 20 threads (0 - 19)
+    for (int i = 0; i < THREAD_COUNT; i++)
     {
+        //create a pointer to be sent to the thread
+        vowelCount *tempCount;
+
+        //assign the pointer to the correct struct in allCounts
+        tempCount = &allCounts[i];
+
+        //create the threads
         cout << "creating thread, " << i << endl;
-        errorCheck = pthread_create(&threads[i], NULL, NULL, (void *) i);
+        errorCheck = pthread_create(&threads[i], NULL, &countTheVowels, (void *) tempCount);
 
         if (errorCheck) {
             cout << "Error: unable to create thread," << errorCheck << endl;
             exit(-1);
         }
     }
-    pthread_exit(NULL);
 }
 
 
